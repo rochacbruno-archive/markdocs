51185bb70160cdf2d75d716f942ff37d98b161f4
Bruno Rocha <rochacbruno@users.noreply.github.com>
2017-08-21 20:54:28 -0300

    Create __init__.py

---
+++ markdocs/__init__.py
@@ -0,0 +1,11 @@
+"""!
+# Why Markdocs
+
+- Markdown is easier to learn
+- Github README file is mostly writen in md format
+- Majority of projects has simple documentation needs (the complex should use .rst)
+
+## Lets make it happen
+
+> markdocs :)
+"""
